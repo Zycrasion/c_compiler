8d707e5f474afdc28e142d22dcd05f4c25f171bd
zycrasion <hendrix.may@gmail.com>
2024-10-28 19:30:27 +1100

    pointer arithmetic works

--- c_test_files/05.c
+++ c_test_files/05.c
@@ -1,7 +1,16 @@
 int main()
 {
-    int a = 20;
-    int* b = &a;
-    int c = *b;
-    return c;
+    int* size = 4;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+
+    // Do something really stupid
+    int* a_ptr = &a;
+    int* b_ptr = a_ptr - size;
+    int* c_ptr = b_ptr - size;
+    int* d_ptr = c_ptr - size;
+
+    return *d_ptr;
 }
